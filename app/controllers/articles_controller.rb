e4994f7874830ad32964301ddd7aa69cbdca7549
Sayak Rana <sayakrana03@gmail.com>
2023-03-31 10:58:12 +0530

    Form to db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,18 @@
 class ArticlesController < ApplicationController
-	def show
-		@article = Article.find(params[:id])
-	end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-	def index
-		@articles = Article.all
-	end
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+   @article = Article.new(params.require(:article).permit(:title, :description))
+   @article.save
+   redirect_to @article
+  end
 end
